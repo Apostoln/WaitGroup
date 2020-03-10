ea77bdc65ae4f61ef9073045a139e42ebf9a7a49
Nik Bond <nikbond97@gmail.com>
2020-03-10 22:47:20 +0200

    Implement switch() as method of SmartWaitGroup; Make it configurable - caller should decide what must be first - waiting or doing

--- src/smart_wait_group.rs
+++ src/smart_wait_group.rs
@@ -2,6 +2,11 @@ use std::sync::Arc;
 
 use crate::wait_group_impl::WaitGroupImpl;
 
+pub enum Order {
+    DoerWaiter,
+    WaiterDoer,
+}
+
 #[derive(Clone)]
 pub struct SmartWaitGroup {
     inner: Arc<WaitGroupImpl>,
@@ -27,6 +32,40 @@ impl SmartWaitGroup {
 
     pub fn counter(&self) -> usize {
         self.inner.counter()
+    }
+
+    pub fn switch_do_wait(&self, second: &SmartWaitGroup) -> Doer {
+        let doer = self.doer();
+        second.waiter().wait();
+        doer
+    }
+
+    pub fn switch_wait_do(&self, second: &SmartWaitGroup) -> Doer {
+        second.waiter().wait();
+        let doer = self.doer();
+        doer
+    }
+
+    pub fn switch(&self, second: &SmartWaitGroup, order: Order) -> Doer {
+        // Ensure that first and second are differ (not an identical allocations)
+        // for avoiding deadlock
+        assert!(!Arc::ptr_eq(&self.inner, &second.inner));
+
+        match order {
+            Order::DoerWaiter => self.switch_do_wait(second),
+            Order::WaiterDoer => self.switch_wait_do(second),
+        }
+    }
+    
+    pub fn switch_unique(&self, second: &SmartWaitGroup) -> Option<Doer> {
+        // Ensure that first and second are differ (not an identical allocations)
+        // for avoiding deadlock
+        assert!(!Arc::ptr_eq(&self.inner, &second.inner));
+        let doer = self.unique_doer();
+        if let Some(_) = doer {
+            second.waiter().wait();
+        }
+        doer
     }
 
     unsafe fn inner(&self) -> Arc<WaitGroupImpl> {
@@ -56,6 +95,7 @@ impl Doer {
         self.wait_group.done();
     }
 }
+
 impl Drop for Doer {
     fn drop(&mut self) {
         self.done();
@@ -80,34 +120,9 @@ impl Waiter {
         self.wait_group.wait();
     }
 }
+
 impl Clone for Waiter {
     fn clone(&self) -> Self {
         Waiter::new(Arc::clone(&self.wait_group))
     }
-}
-
-// todo come up with a good name
-// todo impl MultiWaitGroup
-// todo impl try_switch()
-pub fn switch(first: &SmartWaitGroup, second: &SmartWaitGroup) -> Doer {
-    // Ensure that first and second are differ (not an identical allocations)
-    // for avoiding deadlock
-    assert!(!Arc::ptr_eq(&first.inner, &second.inner));
-    let doer = first.doer();
-    second.waiter().wait();
-    doer
-}
-
-// todo come up with a good name
-// todo impl MultiWaitGroup
-// todo impl try_switch_unique()
-pub fn switch_unique(first: &SmartWaitGroup, second: &SmartWaitGroup) -> Option<Doer> {
-    // Ensure that first and second are differ (not an identical allocations)
-    // for avoiding deadlock
-    assert!(!Arc::ptr_eq(&first.inner, &second.inner));
-    let doer = first.unique_doer();
-    if let Some(_) = doer {
-        second.waiter().wait();
-    }
-    doer
 }
